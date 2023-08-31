95d2597c6fd1167ae6e3e3f58ba7cbb7ec7f04e9
airxd <56568297+MoeNek@users.noreply.github.com>
2023-09-01 00:53:30 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,7 @@ module.exports = {
     title: 'Bird Wiki 喵！',
     url: 'https://bird-wiki.zhiccc.net',
     baseUrl: '/',
+    onBrokenLinks: 'ignore',
     presets: [
         [
           '@docusaurus/preset-classic',
