9aec109a8d6832c4fcbc4912affc68263e36f471
airxd <56568297+MoeNek@users.noreply.github.com>
2023-09-01 00:12:51 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,16 @@ module.exports = {
     title: 'Bird Wiki 喵！',
     url: 'https://bird-wiki.zhiccc.net',
     baseUrl: '/',
+    presets: [
+        [
+          '@docusaurus/preset-classic',
+          {
+            docs: {
+                sidebar: require.resolve('./sidebars.js'),
+            },
+          },
+        ],
+      ],    
     sidebar: [
         {
             type: 'doc',
