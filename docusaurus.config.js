39db63e8e820fbb1e646fbd988bfec69b01ec2a6
airxd <56568297+MoeNek@users.noreply.github.com>
2023-09-01 01:52:13 +0800

    Revert

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     title: 'Bird Wiki 喵！',
     url: 'https://bird-wiki.zhiccc.net',
-    baseUrl: '/docs/',
+    baseUrl: '/',
     onBrokenLinks: 'ignore',
     presets: [
         [
           '@docusaurus/preset-classic',
           {
-            '/': {
+            docs: {
                 sidebarPath: require.resolve('./sidebar.js'),
             },
           },
